5eed22ab665096ae1ba5e3a81dbd32f25e8e5fea
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 12:22:40 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Artycles CLI
+
+# 1. Install the main executable
+npm install -g artycles-cli
+
+#2. Setup Artycles (only needed once)
+artycles setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+artycles -h
